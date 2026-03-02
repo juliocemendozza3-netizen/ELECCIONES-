3d9759c4c750d245e7f17a95258008d182729b31
juliocemendozza3-netizen <juliocemendozza3@gmail.com>
2026-03-01 20:20:18 -0500

    Update robot_e14.py

--- robot_e14.py
+++ robot_e14.py
@@ -1,13 +1,5 @@
 import time
-import requests
 
 while True:
-    print("Robot activo. Probando conexión...")
-
-    try:
-        r = requests.get("https://www.google.com", timeout=10)
-        print("Conexión OK:", r.status_code)
-    except Exception as e:
-        print("Error:", e)
-
+    print("Robot activo...")
     time.sleep(60)
